30fb2bca001a5255e995ad28135866a838306734
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2024-06-05 00:02:19 -0300

    chore:ajuste

---
+++ TestePB.Domain/CasoDeUsos/CriarClienteCasoDeUso.cs
@@ -0,0 +1,28 @@
+﻿using TestePB.Domain.CasoDeUsos.Interfaces;
+using TestePB.Domain.Entity;
+using TestePB.Domain.Interfaces;
+
+namespace TestePB.Domain.CasoDeUsos;
+
+public class CriarClienteCasoDeUso:ICriarClienteCasoDeUso
+{
+    private readonly IClienteRepositorio _clienteRepositorio;
+
+    public CriarClienteCasoDeUso(IClienteRepositorio clienteRepositorio)
+    {
+        _clienteRepositorio = clienteRepositorio;
+    }
+
+    public async Task<Cliente> CriaCliente(
+        string nomeCompleto, 
+        string email, 
+        IEnumerable<Contato> contatos,
+        CancellationToken cancellationToken)
+    {
+
+            var cliente = Cliente.Criar(nomeCompleto, email, contatos);
+            await _clienteRepositorio.Adicionar(cliente,cancellationToken);
+            return cliente;
+
+    }
+}
