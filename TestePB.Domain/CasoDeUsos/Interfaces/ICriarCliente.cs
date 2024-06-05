0f3952a9d17ca48e2a72325380d7165d66bdf641
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2024-06-05 00:04:01 -0300

    refactor:adicionado  cancellationToken

--- TestePB.Domain/CasoDeUsos/Interfaces/ICriarCliente.cs
+++
@@ -1,8 +0,0 @@
-﻿using TestePB.Domain.Entity;
-
-namespace TestePB.Domain.CasoDeUsos.Interfaces;
-
-public interface ICriarCliente
-{
-    Task<Cliente> CriaCliente(string nomeTelefone, string email, IEnumerable<Contatos> contatos);
-}
---
+++ TestePB.Domain/CasoDeUsos/Interfaces/ICriarClienteCasoUso.cs
@@ -0,0 +1,11 @@
+﻿using TestePB.Domain.Entity;
+
+namespace TestePB.Domain.CasoDeUsos.Interfaces;
+
+public interface ICriarClienteCasoDeUso
+{
+    Task<Cliente> CriaCliente(string nomeTelefone, 
+        string email, 
+        IEnumerable<Contato> contatos,
+        CancellationToken cancellationToken);
+}
