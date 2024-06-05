9cd0efee0f7c432e76f69d6e91c835ad3f6db577
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2024-06-05 00:03:25 -0300

    feat:adicionado ObterTodosClienteCasoDeUso.cs

---
+++ TestePB.Domain/CasoDeUsos/Interfaces/IObterTodosClientesCasoDeUso.cs
@@ -0,0 +1,8 @@
+﻿using TestePB.Domain.Entity;
+
+namespace TestePB.Domain.CasoDeUsos.Interfaces;
+
+public interface IObterTodosClientesCasoDeUso
+{
+ Task<IEnumerable<Cliente>> ObterTodosClientes(CancellationToken cancellationToken);
+}
---
+++ TestePB.Domain/CasoDeUsos/ObterTodosClienteCasoDeUso.cs
@@ -0,0 +1,20 @@
+﻿using TestePB.Domain.CasoDeUsos.Interfaces;
+using TestePB.Domain.Entity;
+using TestePB.Domain.Interfaces;
+
+namespace TestePB.Domain.CasoDeUsos;
+
+public class ObterTodosClienteCasoDeUso:IObterTodosClientesCasoDeUso
+{
+    private readonly IClienteRepositorio _clienteRepositorio;
+
+    public ObterTodosClienteCasoDeUso(IClienteRepositorio clienteRepositorio)
+    {
+        _clienteRepositorio = clienteRepositorio;
+    }
+
+    public async Task<IEnumerable<Cliente>> ObterTodosClientes(CancellationToken cancellationToken)
+    {
+        return await _clienteRepositorio.ObterTodos(cancellationToken);
+    }
+}
