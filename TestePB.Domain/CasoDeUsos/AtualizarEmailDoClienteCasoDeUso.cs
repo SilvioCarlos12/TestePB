ededb323468f8ea7a35077e08312c280c9fa9ac0
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2024-06-05 00:00:30 -0300

    feat:adicionado atualizar email do cliente

---
+++ TestePB.Domain/CasoDeUsos/AtualizarEmailDoClienteCasoDeUso.cs
@@ -0,0 +1,27 @@
+﻿using TestePB.Domain.CasoDeUsos.Interfaces;
+using TestePB.Domain.Entity;
+using TestePB.Domain.Interfaces;
+
+namespace TestePB.Domain.CasoDeUsos;
+
+public class AtualizarEmailDoClienteCasoDeUso:IAtualizarEmailClienteCasoDeUso
+{
+    private readonly IClienteRepositorio _clienteRepositorio;
+
+    public AtualizarEmailDoClienteCasoDeUso(IClienteRepositorio clienteRepositorio)
+    {
+        _clienteRepositorio = clienteRepositorio;
+    }
+
+    public async Task<Cliente?> AtualizarEmailDoCliente(Guid id, string email,CancellationToken cancellationToken)
+    {
+        var cliente = await _clienteRepositorio.ObterPorId(id, cancellationToken);
+
+        if (cliente is null)
+            return null;
+        
+        cliente.AtualizarEmailDoCliente(email);
+        
+        return cliente;
+    }
+}
---
+++ TestePB.Domain/CasoDeUsos/Interfaces/IAtualizarEmailClienteCasoDeUso.cs
@@ -0,0 +1,8 @@
+﻿using TestePB.Domain.Entity;
+
+namespace TestePB.Domain.CasoDeUsos.Interfaces;
+
+public interface IAtualizarEmailClienteCasoDeUso
+{
+    Task<Cliente?> AtualizarEmailDoCliente(Guid id, string email,CancellationToken cancellationToken);
+}
